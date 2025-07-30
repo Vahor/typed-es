724df909a5c7d1ba256b139d841615c7a5a0829a
Vahor <me@vahor.fr>
2025-07-30 21:53:11 +0200

    add failing test

---
+++ tests/aggregations/top-hits.test.ts
@@ -0,0 +1,67 @@
+import { describe, test } from "bun:test";
+import { expectTypeOf } from "expect-type";
+import { type ElasticsearchOutput, typedEs } from "../../src/index";
+import { type CustomIndexes, client } from "../shared";
+
+describe("Top Hits Aggregations", () => {
+	test("top_hits query construction", () => {
+		const query = typedEs(client, {
+			index: "orders",
+			_source: false,
+			size: 0,
+			aggs: {
+				top_tags: {
+					terms: {
+						field: "product_ids",
+						size: 3,
+					},
+					aggs: {
+						top_sales_hits: {
+							top_hits: {
+								sort: [
+									{
+										created_at: {
+											order: "desc",
+										},
+									},
+								],
+								_source: {
+									includes: ["shipping_address.street"],
+								},
+								size: 1,
+							},
+						},
+					},
+				},
+			},
+		});
+
+		type Output = ElasticsearchOutput<typeof query, CustomIndexes>;
+		type Aggregations = Output["aggregations"];
+		expectTypeOf<Aggregations>().toEqualTypeOf<{
+			top_tags: {
+				buckets: Array<{
+					key: unknown;
+					doc_count: number;
+					top_sales_hits: {
+						hits: {
+							total: number;
+							max_score: number;
+							hits: Array<{
+								_index: "orders";
+								_id: string;
+								_source: {
+									shipping_address: {
+										street: string;
+									};
+								};
+								sort: Array<unknown>;
+								_score: number | null;
+							}>;
+						};
+					};
+				}>;
+			};
+		}>();
+	});
+});
