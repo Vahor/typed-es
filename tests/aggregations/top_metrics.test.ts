47e5b63294ba458f727508b36af0f127d9d91add
Vahor <me@vahor.fr>
2025-08-01 13:15:41 +0200

    add failing test

---
+++ tests/aggregations/top_metrics.test.ts
@@ -0,0 +1,54 @@
+import { describe, test } from "bun:test";
+import { expectTypeOf } from "expect-type";
+import { type ElasticsearchOutput, typedEs } from "../../src/index";
+import { type CustomIndexes, client } from "../shared";
+
+describe("top_metrics Aggregations", () => {
+	test("get the top 10 of a field", () => {
+		const query = typedEs(client, {
+			index: "orders",
+			size: 0,
+			_source: false,
+			aggs: {
+				max_score: {
+					max: {
+						field: "total",
+					},
+				},
+				top_scores: {
+					top_metrics: {
+						sort: [
+							{
+								total: {
+									order: "desc",
+								},
+							},
+						],
+						metrics: [
+							{
+								field: "total",
+							},
+						],
+						size: 10,
+					},
+				},
+			},
+		});
+		type Output = ElasticsearchOutput<typeof query, CustomIndexes>;
+		type Aggregations = Output["aggregations"];
+		expectTypeOf<Aggregations>().toEqualTypeOf<{
+			max_score: {
+				value: number;
+				value_as_string?: string;
+			};
+			top_scores: {
+				top: Array<{
+					sort: unknown[];
+					metrics: {
+						total: number;
+					};
+				}>;
+			};
+		}>();
+	});
+});
