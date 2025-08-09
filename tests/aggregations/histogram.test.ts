e79d149a4d055cfc0d40bf741c3b5961075a2dd3
Vahor <me@vahor.fr>
2025-08-09 22:17:20 +0200

    add failing test

---
+++ tests/aggregations/histogram.test.ts
@@ -0,0 +1,63 @@
+import { describe, test } from "bun:test";
+import { expectTypeOf } from "expect-type";
+import { type ElasticsearchOutput, typedEs } from "../../src/index";
+import { type CustomIndexes, client } from "../shared";
+
+// https://www.elastic.co/docs/reference/aggregations/search-aggregations-bucket-histogram-aggregation
+describe("Histogram Aggregations", () => {
+	test("default", () => {
+		const query = typedEs(client, {
+			index: "demo",
+			_source: false,
+			size: 0,
+			aggs: {
+				prices: {
+					histogram: {
+						field: "price",
+						interval: 50,
+					},
+				},
+			},
+		});
+		type Output = ElasticsearchOutput<typeof query, CustomIndexes>;
+		type Aggregations = Output["aggregations"];
+		expectTypeOf<Aggregations>().toEqualTypeOf<{
+			price_ranges: {
+				buckets: Array<{
+					key: number;
+					doc_count: number;
+				}>;
+			};
+		}>();
+	});
+
+	test("with keyed", () => {
+		const query = typedEs(client, {
+			index: "demo",
+			_source: false,
+			size: 0,
+			aggs: {
+				prices: {
+					histogram: {
+						field: "price",
+						interval: 50,
+						keyed: true,
+					},
+				},
+			},
+		});
+		type Output = ElasticsearchOutput<typeof query, CustomIndexes>;
+		type Aggregations = Output["aggregations"];
+		expectTypeOf<Aggregations>().toEqualTypeOf<{
+			price_ranges: {
+				buckets: Record<
+					`${number}`,
+					{
+						key: number;
+						doc_count: number;
+					}
+				>;
+			};
+		}>();
+	});
+});
