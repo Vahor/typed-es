9147ba10d7f6b448e6ada22e5039d6f65baa9595
Vahor <me@vahor.fr>
2025-08-16 23:10:51 +0200

    add check to make sure transportOptions are respected

---
+++ tests/client.test.ts
@@ -0,0 +1,53 @@
+import { describe, expectTypeOf, test } from "bun:test";
+import { typedEs } from "../src/index";
+import { client } from "./shared";
+
+const query = typedEs(client, {
+	index: "demo",
+	_source: false,
+});
+
+describe("Client", () => {
+	describe("transport options is respected", () => {
+		describe("search", () => {
+			test("with meta", async () => {
+				type Result = Awaited<
+					ReturnType<typeof client.search<typeof query, { meta: true }>>
+				>;
+				expectTypeOf<Result>().toHaveProperty("body");
+				expectTypeOf<Result>().toHaveProperty("statusCode");
+				expectTypeOf<Result>().toHaveProperty("headers");
+			});
+			test("without meta", async () => {
+				type Result = Awaited<
+					ReturnType<typeof client.search<typeof query, { meta: false }>>
+				>;
+				expectTypeOf<Result>().not.toHaveProperty("body");
+				expectTypeOf<Result>().not.toHaveProperty("statusCode");
+				expectTypeOf<Result>().not.toHaveProperty("headers");
+			});
+		});
+		describe("asyncSearch.get", () => {
+			test("with meta", async () => {
+				type Result = Awaited<
+					ReturnType<
+						typeof client.asyncSearch.get<typeof query, { meta: true }>
+					>
+				>;
+				expectTypeOf<Result>().toHaveProperty("body");
+				expectTypeOf<Result>().toHaveProperty("statusCode");
+				expectTypeOf<Result>().toHaveProperty("headers");
+			});
+			test("without meta", async () => {
+				type Result = Awaited<
+					ReturnType<
+						typeof client.asyncSearch.get<typeof query, { meta: false }>
+					>
+				>;
+				expectTypeOf<Result>().not.toHaveProperty("body");
+				expectTypeOf<Result>().not.toHaveProperty("statusCode");
+				expectTypeOf<Result>().not.toHaveProperty("headers");
+			});
+		});
+	});
+});
