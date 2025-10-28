5c72e6bf29e0b6d1c36af43c8ccaead551696d6b
Vahor <me@vahor.fr>
2025-10-28 14:29:44 +0100

    add sub agg test

--- tests/aggregations/bucket/significant_terms.test.ts
+++ tests/aggregations/bucket/significant_terms.test.ts
@@ -74,4 +74,39 @@ describe("Significant Terms Aggregations", () => {
 			>;
 		}>();
 	});
+
+	test("with avg sub aggregation", () => {
+		type Aggregations = TestAggregationOutput<
+			"demo",
+			{
+				significant_terms_agg: {
+					significant_terms: {
+						field: "entity_id";
+					};
+					aggs: {
+						avg_score: {
+							avg: { field: "score" };
+						};
+					};
+				};
+			}
+		>;
+
+		expectTypeOf<Aggregations["aggregations"]>().toEqualTypeOf<{
+			significant_terms_agg: {
+				doc_count: number;
+				bg_count: number;
+				buckets: Array<{
+					key: string;
+					doc_count: number;
+					score: number;
+					bg_count: number;
+					avg_score: {
+						value: number;
+						value_as_string?: string;
+					};
+				}>;
+			};
+		}>();
+	});
 });
