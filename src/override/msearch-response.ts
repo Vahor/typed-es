ffbc9e20a13f6460000ef80c864f4333aa9376ff
Vahor <me@vahor.fr>
2026-01-27 18:29:30 +0100

    adapt msearch extractor

--- src/override/msearch-response.ts
+++ src/override/msearch-response.ts
@@ -1,6 +1,11 @@
 import type { estypes } from "@elastic/elasticsearch";
 import type { ElasticsearchIndexes, TypedSearchRequest } from "../lib";
-import type { AlternatingPair } from "../types/helpers";
+import type {
+	AlternatingPair,
+	IsNever,
+	Prettify,
+	UnionToIntersection,
+} from "../types/helpers";
 import type { TypedSearchResponse } from "./search-response";
 
 type PairType<E extends ElasticsearchIndexes> = {
@@ -22,33 +27,48 @@ type ExtractAllPairs<Data> = Data extends readonly [
 				Rest extends readonly [unknown, unknown, ...unknown[]] ? Rest : []
 			>,
 		]
-	: [];
+	: Data extends Array<infer T>
+		? IsNever<T> extends true
+			? []
+			: [
+					{
+						// TODO: make this pretty
+						data: PopPair<
+							[
+								"index" extends keyof UnionToIntersection<T>
+									? Pick<UnionToIntersection<T>, "index">
+									: {},
+								Prettify<Omit<UnionToIntersection<T>, "index">>,
+							]
+						>;
+					},
+				]
+		: [];
 
 export type TypedMsearchRequest<Indexes extends ElasticsearchIndexes> = {
 	index: keyof Indexes;
 	searches: MRequestSearches<Indexes>;
 };
 
-export type MRequestSearches<E extends ElasticsearchIndexes> = AlternatingPair<
-	PairType<E>["header"],
-	PairType<E>["request"]
->;
+export type MRequestSearches<E extends ElasticsearchIndexes> =
+	| AlternatingPair<PairType<E>["header"], PairType<E>["request"]>
+	| Array<PairType<E>["header"] | PairType<E>["request"]>;
 
 type ParsedSearches<
 	Query extends TypedMsearchRequest<E>,
 	E extends ElasticsearchIndexes,
 	P = ExtractAllPairs<Query["searches"]>,
 > = {
-	[index in keyof P]: {
-		Index: P[index] extends { data: PairType<E> }
-			? P[index]["data"]["header"]["index"] extends string
-				? P[index]["data"]["header"]["index"]
-				: Query["index"]
+	[index in keyof P]: IsNever<P[index]> extends true
+		? never
+		: P[index] extends { data: PairType<E> }
+			? {
+					Index: P[index]["data"]["header"]["index"] extends string
+						? P[index]["data"]["header"]["index"]
+						: Query["index"];
+					Query: P[index]["data"]["request"];
+				}
 			: never;
-		Query: P[index] extends { data: PairType<E> }
-			? P[index]["data"]["request"]
-			: never;
-	};
 };
 
 export type TypedMSearchResponse<
