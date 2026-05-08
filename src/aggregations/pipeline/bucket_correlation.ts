99561f36173b195e2d78351abfc349846961dc52
Vahor <me@vahor.fr>
2026-05-08 20:16:23 +0200

    Simplify bucket correlation typing

--- src/aggregations/pipeline/bucket_correlation.ts
+++ src/aggregations/pipeline/bucket_correlation.ts
@@ -4,18 +4,7 @@ import type { Prettify } from "../../types/helpers";
  * @see https://www.elastic.co/docs/reference/aggregations/search-aggregations-bucket-correlation-aggregation
  */
 export type BucketCorrelationAggs<Agg> = Agg extends {
-	bucket_correlation: {
-		buckets_path: string;
-		function: {
-			count_correlation: {
-				indicator: {
-					doc_count: number;
-					expectations: readonly number[];
-					fractions?: readonly number[];
-				};
-			};
-		};
-	};
+	bucket_correlation: unknown;
 }
 	? Prettify<{
 			value: number;
